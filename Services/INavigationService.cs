13b8e34b7dce9e69b11dcadba54d8d625b09dfc5
thel719 <levar.howard@pgi.com>
2022-09-20 00:22:43 -0600

    Add project files.

---
+++ Services/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using DXMauiApp2.ViewModels;
+
+namespace DXMauiApp2.Services
+{
+    public interface INavigationService
+    {
+        Task NavigateToAsync<TViewModel>() where TViewModel : BaseViewModel;
+
+        Task NavigateToAsync<TViewModel>(bool isAbsoluteRoute) where TViewModel : BaseViewModel;
+
+        Task NavigateToAsync<TViewModel>(object parameter) where TViewModel : BaseViewModel;
+
+        Task GoBackAsync();
+    }
+}
---
+++ Services/MockDataStore.cs
@@ -0,0 +1,53 @@
+﻿using DXMauiApp2.Models;
+
+namespace DXMauiApp2.Services
+{
+    public class MockDataStore : IDataStore<CalendarEvent>
+    {
+        readonly List<CalendarEvent> items;
+
+        public MockDataStore()
+        {
+            DateTime baseDate = DateTime.Today;
+            items = new List<CalendarEvent>();
+        }
+
+        public async Task<bool> AddItemAsync(CalendarEvent item)
+        {
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(CalendarEvent item)
+        {
+            var oldItem = items.Where((CalendarEvent arg) => arg.Id == item.Id).FirstOrDefault();
+            items.Remove(oldItem);
+            items.Add(item);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            var oldItem = items.Where((CalendarEvent arg) => arg.Id == id).FirstOrDefault();
+            items.Remove(oldItem);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<CalendarEvent> GetItemAsync(string id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<CalendarEvent>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+        public IEnumerable<CalendarEvent> GetItems(bool forceRefresh = false)
+        {
+            return items;
+        }
+    }
+}
