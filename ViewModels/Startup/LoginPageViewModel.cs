90a96ac780f7908dfda1deb75cc180be0527ff7a
Josue <josuetorresf2@MacBook-Pro.local>
2022-09-21 13:16:00 -0600

    finally an app working, I did not push it at first because it was broken, but now it works

---
+++ ViewModels/Startup/LoginPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Newtonsoft.Json;
+using SimpleLoginUI.Controls;
+using SimpleLoginUI.Models;
+using SimpleLoginUI.Views.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLoginUI.ViewModels.Startup
+{
+    public partial class LoginPageViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        private string _email;
+
+        [ObservableProperty]
+        private string _password;
+
+        #region Commands
+        [ICommand]
+        async void Login()
+        {
+            if (!string.IsNullOrWhiteSpace(Email) && !string.IsNullOrWhiteSpace(Password))
+            {
+                var userDetails = new UserBasicInfo();
+                userDetails.Email = Email;
+                userDetails.FullName = "Test User Name";
+
+                // Student Role, Teacher Role, Admin Role,
+                if (Email.ToLower().Contains("student"))
+                {
+                    userDetails.RoleID = (int)RoleDetails.Student;
+                    userDetails.RoleText = "Student Role";
+                }
+                else if (Email.ToLower().Contains("teacher"))
+                {
+                    userDetails.RoleID = (int)RoleDetails.Teacher;
+                    userDetails.RoleText = "Teacher Role";
+                }
+                else
+                {
+                    userDetails.RoleID = (int)RoleDetails.Admin;
+                    userDetails.RoleText = "Admin Role";
+                }
+
+
+                // calling api 
+
+
+                if (Preferences.ContainsKey(nameof(App.UserDetails)))
+                {
+                    Preferences.Remove(nameof(App.UserDetails));
+                }
+
+                string userDetailStr = JsonConvert.SerializeObject(userDetails);
+                Preferences.Set(nameof(App.UserDetails), userDetailStr);
+                App.UserDetails = userDetails;
+                await AppConstant.AddFlyoutMenusDetails();
+            }
+
+
+        }
+        #endregion
+    }
+}
---
+++ obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("SimpleLoginUI.MainApplication, SimpleLoginUI, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6459c895fb3ef9f1af.MainApplication.class, crc6459c895fb3ef9f1af.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
