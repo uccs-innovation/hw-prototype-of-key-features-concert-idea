4e29ddc16117fa6b6bb287a760f032cdab2b6b31
Nick Petik <thelaugher1@gmail.com>
2022-09-21 17:44:51 -0700

    Created View Model

---
+++ ViewModels/CalendarViewModel.cs
@@ -0,0 +1,17 @@
+
+
+namespace StudyN.ViewModels;
+
+public class CalendarViewModel : BaseViewModel
+{
+	public CalendarViewModel()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
